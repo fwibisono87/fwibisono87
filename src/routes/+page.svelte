4a5ffb3623a00cf49a7de547c7ebc9ad7025b094
Francis Wibisono <code@franciswibisono.com>
2022-12-20 01:25:55 +0700

    spike: poc data section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,7 +6,10 @@
 	let scrollY: number = 0;
 </script>
 
-<div class="min-h-screen min-w-screen flex flex-col pt-20 justify-between {scrollY!==0 && 'mb-[6.5rem] lg:mb-[4rem]'}">
+<div
+	class="min-h-screen min-w-screen flex flex-col pt-20 justify-between {scrollY !== 0 &&
+		'mb-[6.5rem] lg:mb-[4rem]'}"
+>
 	<div class="mx-auto text-center my-auto" transition:fade>
 		<div class="flex flex-col gap-2 -mt-24">
 			<img src="/img/anime.webp" class="w-24 aspect-square rounded-full mx-auto" />
@@ -19,13 +22,26 @@
 		</div>
 	</div>
 	{#if scrollY === 0}
-		<div class="mx-auto mb-10 lg:mb-0" in:fade={{ duration: 200 }} out:fade>
+		<div class="mx-auto mb-10 lg:mb-0" in:fade={{ delay: 500, duration: 200 }} out:fade>
 			<MaterialSymbolsKeyboardArrowDownRounded class="w-16 h-16 animate-pulse animate-bounce" />
 		</div>
 	{/if}
 </div>
-{#if scrollY !==0}
-	<div class="mx-auto" in:fade={{ delay: 500, duration: 200 }} out:fade>nyaaaho</div>
+{#if scrollY !== 0}
+	<div
+		class="mx-auto -mt-60 lg:-mt-80 w-full lg:w-[80vw]"
+		in:fade={{ delay: 500, duration: 200 }}
+		out:fade
+	>
+		<div class="flex flex-col">
+			<h3 class="text-2xl font-semibold text-dove text-center lg:text-left">Technologies</h3>
+			<div class="grid grid-cols-1 lg:grid-cols-3 justify-around text-center gap-y-8 mt-6 lg:mt-2">
+				<span in:fade={{ delay: 800, duration: 200 }} out:fade>ini satu</span>
+				<span in:fade={{ delay: 1100, duration: 200 }} out:fade>ini dua</span>
+				<span in:fade={{ delay: 1400, duration: 200 }} out:fade>ini tiga</span>
+			</div>
+		</div>
+	</div>
 {/if}
 
 <svelte:window bind:scrollY />
